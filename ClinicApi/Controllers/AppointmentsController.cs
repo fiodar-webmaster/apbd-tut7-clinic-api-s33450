e6dce05a038ab74ef9f67e58f6647c8a3efe23b4
fiodar-webmaster <murasakibara314@gmail.com>
2026-04-25 15:50:44 +0200

    Set up configuration, create DTOs

---
+++ ClinicApi/Controllers/AppointmentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clinic_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentsController : ControllerBase
+{
+    // [HttpGet]
+    // public IActionResult Get() 
+}
---
+++ ClinicApi/DTOs/ErrorResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace clinic_api.DTOs;
+
+public class ErrorResponseDto
+{
+    public string ErrorCode { get; set; }
+    public string Message { get; set; }
+    public Dictionary<string, string> Details { get; set; }
+}
