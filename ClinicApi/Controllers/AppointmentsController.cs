d23a4c777acc85cbb9f084dc53d5299581218448
fiodar-webmaster <murasakibara314@gmail.com>
2026-04-25 17:54:03 +0200

    Implement GetAppointments method in AppointmentsController

--- ClinicApi/Controllers/AppointmentsController.cs
+++ ClinicApi/Controllers/AppointmentsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using clinic_api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace clinic_api.Controllers;
 
@@ -6,6 +8,61 @@ namespace clinic_api.Controllers;
 [Route("api/[controller]")]
 public class AppointmentsController : ControllerBase
 {
-    // [HttpGet]
-    // public IActionResult Get() 
+    private readonly IConfiguration _configuration;
+
+    public AppointmentsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAppointments(string? status, string? patientLastName)
+    {
+        var appointments = new List<AppointmentListDto>();
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        await using var connection = new SqlConnection(connectionString);
+
+        var sql = @"
+            SELECT
+                a.IdAppointment,
+                a.AppointmentDate,
+                a.Status,
+                a.Reason,
+                p.FirstName + N' ' + p.LastName AS PatientFullName,
+                p.Email AS PatientEmail
+            FROM dbo.Appointments a
+            JOIN  dbo.Patients p ON  a.IdPatient = p.IdPatient
+            WHERE (@Status IS NULL OR a.Status = @Status)
+            AND (@PatientLastName IS NULL OR p.LastName = @PatientLastName)
+            ORDER BY a.AppointmentDate DESC;";
+        
+        await using var command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@Status", string.IsNullOrEmpty(status)? DBNull.Value : status);
+        command.Parameters.AddWithValue("@PatientLastName", string.IsNullOrEmpty(patientLastName)? DBNull.Value : patientLastName);
+
+
+        await connection.OpenAsync();
+        await using var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            appointments.Add(new AppointmentListDto
+            {
+                IdAppointment = reader.GetInt32(reader.GetOrdinal("IdAppointment")),
+                AppointmentDate = reader.GetDateTime(reader.GetOrdinal("AppointmentDate")),
+                Status = reader.GetString(reader.GetOrdinal("Status")),
+                Reason = reader.GetString(reader.GetOrdinal("Reason")),
+                PatientFullName = reader.GetString(reader.GetOrdinal("PatientFullName")),
+                PatientEmail = reader.GetString(reader.GetOrdinal("PatientEmail"))
+            });
+            }
+        return Ok(appointments);
+        }
+
+    [HttpGet("{idAppointment}")]
+    public async Task<IActionResult> GetAppointment(int idAppointment)
+    {
+        
+    }
+    
 }
